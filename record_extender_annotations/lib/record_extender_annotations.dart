9afffe862d8405a8889075521f1a720af4102a2b
Alexander Gutev <alex.gutev@gmail.com>
2024-02-25 13:37:52 +0100

    Add annotations for record extensions.

---
+++ record_extender_annotations/lib/record_extender_annotations.dart
@@ -0,0 +1,90 @@
+library record_extender_annotations;
+
+/// Annotation for generating record type extensions.
+///
+/// This annotation is applicable to extensions. When applied it specifies
+/// that the extension should be used as template for generating extensions
+/// on record types.
+///
+/// [size] record extensions are generated with the number of elements varying
+/// from 2 to [size].
+///
+/// For example:
+///
+/// ```dart
+/// @RecordExtension(size: 3)
+/// extension MyExtension<T> on MyType<T> {
+/// ...
+/// }
+/// ```
+///
+/// Specifies that the following record extensions should be generated:
+///
+/// ```dart
+/// extension MyExtension2<T$1, T$2> on (MyType<T$1>, MyType<T$2> {
+/// ...
+/// }
+///
+/// extension MyExtension3<T$1, T$2, T$3> on (
+///   MyType<T$1>,
+///   MyType<T$2>,
+///   MyType<T$3>
+/// ) {
+/// ...
+/// }
+/// ```
+class RecordExtension {
+  /// Number of record extensions to generate
+  final int size;
+
+  /// Specifies that [size] record extensions should be generated using the annotated element as a template.
+  const RecordExtension({
+    required this.size
+  });
+}
+
+/// Specify that the annotated element should be included in a generated record extension.
+///
+/// This annotation is applicable to elements within an extension annotated
+/// with [RecordExtension]. When applied it specifies that the annotated
+/// element should be included in all generated record extensions for the
+/// extension in which the element is defined.
+///
+/// This annotation defines the return type ([type]) and the code
+/// ([implementation]) serving as the implementation.
+///
+/// Within [type] and [implementation] the following placeholders can be used:
+///
+/// * `{types}`
+///
+///   Substituted with the comma-separated list of the record element types,
+///   e.g. 'MyType<T$1>, MyType<T$1>, MyType<T$3>'.
+///
+/// * `{elements}`
+///
+///   Substituted with the comma-separated list of accessors for each element,
+///   e.g. '$1, $2, $3'.
+///
+/// * `{type-params}`
+///
+///   Substitude with the comma-separated List of the generated extension type
+///   parameters, e.g. 'T$1, T$2, T$3'.
+///
+/// The following elements are supported:
+///
+/// - Property getters
+class RecordExtensionElement {
+  /// The return type and implementation code of this element
+  final String type, implementation;
+
+  /// Specify that the annotated element should be included in a generated record extension.
+  ///
+  /// [type] is the code defining the return type of the generated element.
+  ///
+  /// [implementation] is the code defining the implementation of the
+  /// generated element.
+  const RecordExtensionElement({
+    required this.type,
+    required this.implementation
+  });
+}
